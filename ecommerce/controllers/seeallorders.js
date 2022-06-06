2cc5e002f773b65634fd339e6a307340f4b69957
sumon2017 <zksumon2017@gmail.com>
2022-06-07 05:34:31 +0600

    initial commit

---
+++ ecommerce/controllers/seeallorders.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const orders = require(path.join(__dirname,'..','DbSchemas','orders'));
+
+const myorders = async ( req , res ) => {
+
+    const  email  = res.locals.email;
+    if(email != "admin"){
+        res.json({
+            msg : "you are not admin"
+        });
+    }
+    let all_my_orders = await orders.find({});
+
+    res.json(all_my_orders);
+
+};
+
+module.exports=myorders;
